7fb31a0f417830952fc14098db03eeecfa3b99f6
vlicecream <vlicecream@163.com>
2026-05-08 10:46:32 +0800

    Enable completion debug by default

--- lua/ucore/config.lua
+++ lua/ucore/config.lua
@@ -278,7 +278,7 @@ local function default_values()
 		completion = {
 			min_chars = 2,
 			debounce_ms = 180,
-			debug = false,
+			debug = true,
 		},
 
 		-- UCore diagnostics rendered through vim.diagnostic.
